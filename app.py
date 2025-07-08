794d6edc1ef0dae967c21aa6b67351c886e860d8
MostafaMergaz1 <m9058354@gmail.com>
2025-07-08 15:38:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 import streamlit as st
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 from sklearn.model_selection import train_test_split
